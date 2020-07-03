a1fa7a0e638a9932221527704d7d4bc8c10eb4e3
ckwan-hub <ckwandrans@gmail.com>
2020-07-03 15:04:44 -0400

    GUI foundation set - DO NOT USE THIS REVISION

--- Form-Filler.py
+++ Form-Filler.py
@@ -16,26 +16,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 
-# Browser instance, navigation and check for correct page Title
-driver = webdriver.Chrome()
-driver.get("https://subscribe2.buffalonews.com/register")
-time.sleep(2)
-pageTitle = driver.title
-print(pageTitle)
-assert "Subscribe to The Buffalo News" in driver.title
-print('Correct Page')
-
-
 # print(pyautogui.size())
 
 # window settings
-# window = tkinter.Tk()
-# window.configure(background='black')
-# window.title('Form-Filler')
-# window.geometry('400x300')
-# window.resizable(0, 0)
+window = tkinter.Tk()
+window.configure(background='black')
+window.title('Form-Filler')
+window.geometry('400x230')
+window.resizable(0, 0)
 
-# web fields
+# Create parent frame
+parentFrame = tkinter.Frame(width=220, height=230,
+                            borderwidth=2, relief='groove')
+parentFrame.grid(row=0, column=0, rowspan=2)
+parentFrame.grid_rowconfigure((0, 11), weight=1)
+parentFrame.grid_columnconfigure((0, 1), weight=1)
+parentFrame.grid_propagate(0)
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=1)
+dropdownFont = ('tahoma', 8)
+
+# Create walkthrough frame
+secondFrame = tkinter.Frame(width=180, height=170,
+                            borderwidth=2, relief='groove')
+secondFrame.grid(row=0, column=1)
+secondFrame.grid_propagate(0)
+secondFrame.grid_columnconfigure((0, 1), weight=1)
+
+# Create run button frame
+runFrame = tkinter.Frame(
+    width=180, height=60, borderwidth=2, relief='groove')
+runFrame.grid(row=1, column=1)
+runFrame.grid_propagate(0)
+runFrame.grid_columnconfigure((0, 1), weight=1)
+
+# web fields variables
 # TODO: Open Sharklasers.com, get field with email, assign variable to userEmail, then proceed
 userEmail = ""
 userPassword = "Ab111111"
@@ -44,30 +59,38 @@ lName = "UserLast"
 
 # tkinter functions
 
-# Defining and locating elements
-
-
-# def login(url, email, password, passconfirm, fname, lname):
-driver.find_element_by_name("email").send_keys(userEmail)
-driver.find_element_by_name("pw").send_keys(userPassword)
-driver.find_element_by_name("pwConf").send_keys(userPassword)
-driver.find_element_by_name("firstName").send_keys(fName)
-driver.find_element_by_name("lastName").send_keys(lName)
-driver.find_element_by_name("lastName").send_keys(Keys.RETURN)
-# driver.find_element_by_class_name(MuiButtonBase-root).click()
-# driver.find_element_by_class_name(MuiButtonBase-root).submit()
-
-
-def get_clear_browsing_button(driver):
-    return driver.find_element_by_css_selector('* /deep/ #clearBrowsingDataConfirm')
-
-
+# Browser instance, navigation and check for correct page Title
+# infoBox.set('Opening Window...')
+driver = webdriver.Chrome()
+driver.get("https://subscribe2.buffalonews.com/register")
+# infoBox.set('Checking Correct Page...')
 time.sleep(2)
-print("Fields Entered, Checking For Confirmation...")
-assert "check your email to confirm" in driver.page_source
+pageTitle = driver.title
+print(pageTitle)
+assert "Subscribe to The Buffalo News" in driver.title
+print('Correct Page!')
+print('Standby For User Input...')
 
-# Clear the cache after submission
-print("Removing Cookies...")
-driver.delete_all_cookies()
-print("Cookies Deleted")
-print("Completed!")
+
+# Defining and locating elements
+def loginFunction():
+    print("Running Main Function...")
+    driver.find_element_by_name("email").send_keys(userEmail)
+    driver.find_element_by_name("pw").send_keys(userPassword)
+    driver.find_element_by_name("pwConf").send_keys(userPassword)
+    driver.find_element_by_name("firstName").send_keys(fName)
+    driver.find_element_by_name("lastName").send_keys(lName)
+    driver.find_element_by_name("lastName").send_keys(Keys.RETURN)
+    # driver.find_element_by_class_name(MuiButtonBase-root).click()
+    # driver.find_element_by_class_name(MuiButtonBase-root).submit()
+    time.sleep(2)
+    print("Fields Entered, Checking For Confirmation...")
+    assert "check your email to confirm" in driver.page_source
+    # Clear the cache after submission
+    print("Removing Cookies...")
+    driver.delete_all_cookies()
+    print("Cookies Deleted")
+    print("Completed!")
+
+
+window.mainloop()
